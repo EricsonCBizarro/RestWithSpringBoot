7714bb9206228a9e9fa48950e96bbd842263974c
Ericson.Bizarro <ericsonbizarro@gmail.com>
2020-02-10 14:49:11 -0300

    Crud Person

--- PersonSpringBootApi/src/main/java/br/com/erudio/PersonSpringBootApi/controller/PersonController.java
+++ PersonSpringBootApi/src/main/java/br/com/erudio/PersonSpringBootApi/controller/PersonController.java
@@ -1,14 +1,10 @@
 package br.com.erudio.PersonSpringBootApi.controller;
 
-import br.com.erudio.CalculadoraSpringBootApi.math.SimpleMath;
 import br.com.erudio.PersonSpringBootApi.model.Person;
 import br.com.erudio.PersonSpringBootApi.services.PersonServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -36,7 +32,37 @@ class PersonController {
             )
     public Person findById(@PathVariable("id") String id) {
         return services.findById(id);
+
+
     }
 
+    @RequestMapping
+            (
+                    method = RequestMethod.POST,
+                    consumes = MediaType.APPLICATION_JSON_VALUE,
+                    produces = MediaType.APPLICATION_JSON_VALUE
+            )
+    public Person create(@RequestBody Person person) {
+        return services.create(person);
+    }
+
+    @RequestMapping
+            (
+                    method = RequestMethod.PUT,
+                    consumes = MediaType.APPLICATION_JSON_VALUE,
+                    produces = MediaType.APPLICATION_JSON_VALUE
+            )
+    public Person update(@RequestBody Person person) {
+        return services.update(person);
+    }
+
+    @RequestMapping
+            (
+                    value = "/{id}",
+                    method = RequestMethod.DELETE
+            )
+    public void delete(@RequestBody String id) {
+        services.delete(id);
+    }
 
 }
